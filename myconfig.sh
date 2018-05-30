a276960f6663c90702807105858af1c121f8cb9b
Dan Huang <dan.e.huang@gmail.com>
2018-05-30 14:02:06 -0700

    cleanup

--- myconfig.sh
+++
@@ -1,3 +0,0 @@
-export PATH="/Users/dehuang/Documents/research/pnh/coq/build/bin":$PATH
-export COQ_MK="/Users/dehuang/Documents/research/pnh/coq/build/bin/coq_makefile"
-export COQBIN="/Users/dehuang/Documents/research/pnh/coq/build/bin/"
